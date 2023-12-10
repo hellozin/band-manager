4e1f787529b8f1f164f7880720c490cc79f1ddd3
hellozin <paul26375@gmail.com>
2023-12-10 14:00:13 +0900

    프로젝트 생성

---
+++ src/main/java/me/hellozin/bandmanager/BandManagerApplication.java
@@ -0,0 +1,13 @@
+package me.hellozin.bandmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hellozin/bandmanager/BandManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hellozin.bandmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
